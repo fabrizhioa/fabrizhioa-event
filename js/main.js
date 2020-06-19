3ed0ee4e176e371f50e009fa0e72a4669a706676
Fabrizhio Al chariti <35412190+fabrizhioa@users.noreply.github.com>
2020-06-19 19:27:13 -0400

    master
    all modify

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
-window.onload = (e) => {
-    let deferredPrompt;
-    window.addEventListener('beforeinstallprompt', (e) => {
-        // Prevent Chrome 67 and earlier from automatically showing the prompt
-        e.preventDefault();
-        // Stash the event so it can be triggered later.
-        deferredPrompt = e;
-    });
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-        .then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-        });
-}
-
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('./sw.js')
@@ -29,6 +7,11 @@ if ('serviceWorker' in navigator) {
             .catch((e) => {
                 console.log('Service Worker Failed :(', e)
             })
+    })
+    let deferredPrompt;
+    window.addEventListener('beforeinstallprompt', (e)=>{
+        e.preventDefault();
+        deferredPrompt = e;
     })
 }
 
